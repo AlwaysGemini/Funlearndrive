230faffc47bb6e26b524bcc4414436b120244e24
185547890 <185547890@qq.com.com>
2018-09-18 22:23:06 +0800

    初步完成评价界面

--- app/src/main/java/com/example/administrator/funlearndrive/Navigation_video_fragment.java
+++ app/src/main/java/com/example/administrator/funlearndrive/Navigation_video_fragment.java
@@ -1,8 +1,8 @@
 package com.example.administrator.funlearndrive;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +12,15 @@ import android.view.ViewGroup;
  */
 
 public class Navigation_video_fragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.navigation_video_fragment,null);
+        View view = inflater.inflate(R.layout.navigation_video_fragment, null);
+
 
         return view;
     }
+
+
 }
